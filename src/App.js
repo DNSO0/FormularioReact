3d780720d1f2ef53871f660e491e284ba420c8c2
DNSO0 <toxicthrashd@gmail.com>
2024-12-28 13:10:38 -0500

    remplazo de imputs por componentes de material-ui

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import FormSighUp from './Componentes/FormSignUp';
+import Container from '@mui/material/Container';
+import Typography from '@mui/material/Typography';
 
 function App() {
   return (
-    < >
-      <h1>Formulario Registro</h1>
+    <Container component="section" maxWidth="sm">
+      <Typography variant='h3' component='h1' align='center'>Formulario Registro</Typography>
       <FormSighUp />
-    </>
+    </Container>
   );
 }
 
