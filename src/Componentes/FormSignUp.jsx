f93eab8d90e4d078e0c8d2baa1505b2f4da4c967
DNSO0 <toxicthrashd@gmail.com>
2024-12-28 11:30:34 -0500

    instalacion de libreria material-ui, se agregan componentes basicos al formulario

--- src/Componentes/FormSignUp.jsx
+++ src/Componentes/FormSignUp.jsx
@@ -1,8 +1,19 @@
+import Button from '@mui/material/Button'
+
 function FormSignUp() {
-    return <Form>
+    return <form>
+        <label >Nombre</label>
         <input type="text" />
-        <button>Registrarse</button>
-    </Form>
+        <label >Apellidos</label>
+        <input type="text" />
+        <label >Correo electronico</label>
+        <input type="email" />
+        <label >Promociones</label>
+        <input type="checkbox" />
+        <label >Novedades</label>
+        <input type="checkbox" />
+        <Button variant="contained" >Registrarse</Button>
+    </form>
 }
 
-export default FormSighUp
+export default FormSignUp
