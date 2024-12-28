2262a173727dff698168e9a6aa66ed2788e76172
DNSO0 <toxicthrashd@gmail.com>
2024-12-28 14:55:55 -0500

    Implementación de formularios controlados con manejo de eventos

--- src/Componentes/FormSignUp.jsx
+++ src/Componentes/FormSignUp.jsx
@@ -1,42 +1,80 @@
-import Button from '@mui/material/Button';
-import TextField from '@mui/material/TextField';
-import  Switch  from '@mui/material/Switch';
-import FormGroup from '@mui/material/FormGroup';
-import FormControlLabel from '@mui/material/FormControlLabel';
+import { useState } from "react";
+import Button from "@mui/material/Button";
+import TextField from "@mui/material/TextField";
+import Switch from "@mui/material/Switch";
+import FormGroup from "@mui/material/FormGroup";
+import FormControlLabel from "@mui/material/FormControlLabel";
 
 function FormSignUp() {
-    return <form>
-        
-        <TextField 
-            id="name" 
-            label="Nombre" 
-            variant="outlined" 
-            fullWidth
-            margin="normal"
+  const [name, setName] = useState("");
+  const [lastName, setLasName] = useState("");
+  const [email, setEmail] = useState("");
+  const [prom, setProm] = useState(true);
+  const [nov, setNov] = useState(false);
+
+  return (
+    <form
+      onSubmit={(e) => {
+        e.preventDefault();
+        console.log({
+          name,
+          lastName,
+          email,
+          prom,
+          nov,
+        });
+      }}
+    >
+      <TextField
+        id="name"
+        label="Nombre"
+        variant="outlined"
+        fullWidth
+        margin="normal"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+      />
+      <TextField
+        id="lastName"
+        label="Apellidos"
+        variant="outlined"
+        fullWidth
+        margin="normal"
+        value={lastName}
+        onChange={(e) => setLasName(e.target.value)}
+      />
+      <TextField
+        id="email"
+        label="Email"
+        variant="outlined"
+        fullWidth
+        margin="normal"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+      />
+
+      <FormGroup>
+        <FormControlLabel
+          control={
+            <Switch
+              checked={prom}
+              onChange={(e) => setProm(e.target.checked)}
+            />
+          }
+          label="Promociones"
         />
-        <TextField 
-            id="lastName" 
-            label="Apellidos" 
-            variant="outlined" 
-            fullWidth
-            margin="normal"
+        <FormControlLabel
+          control={
+            <Switch checked={nov} onChange={(e) => setNov(e.target.checked)} />
+          }
+          label="Novedades"
         />
-        <TextField 
-            id="email" 
-            label="Email" 
-            variant="outlined" 
-            fullWidth
-            margin="normal"
-        />        
-        
-        <FormGroup>
-            <FormControlLabel control={<Switch defaultChecked/>} label="Promociones" />
-            <FormControlLabel control={<Switch defaultChecked/>} label="Novedades" />
-        
-        
-        </FormGroup>
-        <Button variant="contained" >Registrarse</Button>
+      </FormGroup>
+      <Button variant="contained" type="submit">
+        Registrarse
+      </Button>
     </form>
+  );
 }
 
-export default FormSignUp
+export default FormSignUp;
