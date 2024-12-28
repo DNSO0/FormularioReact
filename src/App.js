9544107c2ed882207182a195ced12853aaf57263
DNSO0 <toxicthrashd@gmail.com>
2024-12-28 11:01:52 -0500

    commit inicial, estructura base del formulario

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import FormSighUp from './Componentes/FormSignUp';
+
+function App() {
+  return (
+    < >
+      <h1>Formulario Registro</h1>
+      <FormSighUp />
+    </>
+  );
+}
+
+export default App;
---
+++ src/Componentes/FormSignUp.jsx
@@ -0,0 +1,8 @@
+function FormSignUp() {
+    return <Form>
+        <input type="text" />
+        <button>Registrarse</button>
+    </Form>
+}
+
+export default FormSighUp
